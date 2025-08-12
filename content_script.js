7013d40224404c7f5dd7b8479211ae7ce582dd72
Amalia Haible <florian@haible.de>
2025-08-12 05:49:34 +0200

    removed all the useless template stuff. added explanation comment

--- content_script.js
+++ content_script.js
@@ -1,4 +1,18 @@
-// Put all the javascript code here, that you want to execute after page load.
+
+// Core idea:
+// When watching with dub, i always wonder where i know that voice from.
+// This extension allows me to mark roles as recognizable
+// And mark movies/series as seen
+// I dont want to always show everything, thats why i also mark specific roles
+// So i can mark something like Peter Griffin without marking everyone that every spoke something in Family Guy
+//
+// Technical 
+// I keep track of a list of movie/series ids
+// and of specific roles, as [germanSpeaker, roleName, series/movie_id]
+// Displaying the roles is easy, but getting all movies/series is more annoying, so i just check their profile
+// I display everything in a small popup table
+// This is my first extension and for parts of it i got some AI help 
+// but thats just because i never worked on extensions/javascript at all
 
 const isMovie = window.location.pathname.includes("/film/");
 const isSeries = window.location.pathname.includes("/serie/");
@@ -9,48 +23,17 @@ const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 async function createMediaStar() {
   const header = document.querySelector("h1");
   const star = document.createElement("span");
-  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   let media = await getMedia();
-  // if (media && media.includes(window.location.pathname)) {
-  //   svg.setAttribute("fill", "yellow");
-  // } else {
-  //   svg.setAttribute("fill", "none"); // default fill
-  // }
-  // console.log(svg);
-  // svg.setAttribute("width", "24");
-  // svg.setAttribute("height", "24");
-  // svg.setAttribute("viewBox", "0 0 24 24");
-  // svg.setAttribute("stroke", "black");
-  // svg.setAttribute("stroke-width", "2");
-  //
-  // const polygon = document.createElementNS(
-  //   "http://www.w3.org/2000/svg",
-  //   "polygon",
-  // );
-  // polygon.setAttribute(
-  //   "points",
-  //   "12,2 15,11 24,11 17,16 20,24 12,19 4,24 7,16 0,11 9,11",
-  // );
-  //
-  // svg.appendChild(polygon);
-  // svg.id = "media-star";
-  // svg.dataset.mediaLink = window.location.pathname;
-  // console.log(svg.dataset.mediaLink );
-  // svg.addEventListener("click", callbackToggleMediaFavorite);
-  // header.insertBefore(svg, header.firstChild);
-  // console.log(svg);
 
   star.className = "media-star";
   star.innerHTML = "☆";
   if (media && media.includes(window.location.pathname)) {
     star.innerHTML = "★";
   }
-  // header.appendChild(star);
   star.dataset.mediaLink = window.location.pathname;
 
   star.addEventListener("click", callbackToggleMediaFavorite);
   header.insertBefore(star, header.firstChild);
-  // console.log(star);
 }
 
 async function createSpeakerStars() {
@@ -91,10 +74,8 @@ async function createSpeakerStars() {
       }
     }
 
-    // speakerCell.appendChild(star);
     speakerCell.insertBefore(star, speakerCell.firstChild);
 
-    // Add hover tooltip
     speakerLink.addEventListener("mouseenter", (e) => {
       showSpeakerTooltip(e, speakerId);
     });
@@ -102,7 +83,6 @@ async function createSpeakerStars() {
       hideTooltip();
     });
 
-    // Click handler
     star.addEventListener("click", callbackToggleSpeakerFavorite);
   }
 }
@@ -110,7 +90,6 @@ async function createSpeakerStars() {
 async function showSpeakerTooltip(e, speakerId) {
   hideTooltipInstant(); // Remove existing tooltip
 
-  // Create tooltip
   const tooltip = document.createElement("div");
   tooltip.className = "speaker-tooltip";
   let content = "";
@@ -121,7 +100,6 @@ async function showSpeakerTooltip(e, speakerId) {
     for (const line of data) {
       content += `<tr><td>${line[0]}</td><td><a href=${line[2]}>${line[1]}</a></td></tr>`;
     }
-    // content += `</tbody></table>`;
   }
 
   if (e.shiftKey) {
@@ -208,20 +186,14 @@ async function showSpeakerTooltip(e, speakerId) {
 async function parseListElement(element) {
   let rolename = element.querySelector("em");
   if (rolename) {
-    // console.log("rolename ", rolename["textContent"]);
     let links = element.querySelectorAll("a");
     for (let vlink of links) {
-      // console.log("link ", vlink, vlink.href);
       if (vlink.href.includes("/film") || vlink.href.includes("/serie")) {
-        // console.log("rolename, link", rolename, vlink);
         return [rolename["textContent"], vlink["href"], vlink["innerText"]];
       }
     }
-    console.log("OOOPS");
+    console.log("Synchronkartei Highlighter: Could not parse row.", element);
   } else {
-    // console.log(element);
-    // console.log("NOT FOUND, ", element.firstChild["textContent"].trim().slice(4).split("\n")[0], element.firstChild);
-    // console.log(element.children[0]);
     return [
       element.firstChild["textContent"].trim().slice(4).split("\n")[0],
       element.children[0]["href"],
@@ -230,7 +202,6 @@ async function parseListElement(element) {
   }
 }
 
-// Hide tooltip
 async function hideTooltip() {
   const tooltip = document.querySelector(".speaker-tooltip");
   await delay(500);
@@ -253,14 +224,10 @@ async function callbackToggleSpeakerFavorite(event) {
   } else {
     event.target.innerHTML = "☆";
   }
-
-  // let data = await getSpeaker(event.target.dataset.speakerId);
-  // console.log(data);
 }
 
 async function callbackToggleMediaFavorite(event) {
   let res = await toggleMediaEntry(event.target.dataset.mediaLink);
-  // console.log("callbackToggleMediaFavorite res", res);
   if (res) {
     event.target.innerHTML = "★";
   } else {
@@ -269,19 +236,17 @@ async function callbackToggleMediaFavorite(event) {
 }
 async function toggleSpeakerEntry(speaker, entry) {
   if (!speaker || !entry) {
-    console.log("ToggleSpeakerEntry called invalidly", speaker, entry);
+    console.log("Synchronkartei Highlighter: ToggleSpeakerEntry called invalidly", speaker, entry);
     return;
   }
   let store = await browser.storage.local.get("lists");
   let lists = store.lists || {};
-  // console.log("store: " + store, " lists: ", lists);
 
   if (!lists[speaker]) {
     lists[speaker] = [];
   }
 
   const sublistStr = JSON.stringify(entry);
-  // console.log(lists[speaker]);
   const index = lists[speaker].findIndex(
     (s) => JSON.stringify(s) === sublistStr,
   );
@@ -292,10 +257,9 @@ async function toggleSpeakerEntry(speaker, entry) {
   } else {
     lists[speaker].splice(index, 1);
     if (lists[speaker].length === 0) {
-      delete lists[speaker]; // optional cleanup
+      delete lists[speaker];
     }
   }
-  // console.log("ToggleSpeakerEntry, new state:", lists[speaker]);
 
   await browser.storage.local.set({ lists });
   return res;
@@ -303,10 +267,9 @@ async function toggleSpeakerEntry(speaker, entry) {
 
 async function toggleMediaEntry(entry) {
   if (!entry) {
-    console.log("ToggleMediaEntry called invalidly", entry);
+    console.log("Synchronkartei Highlighter: ToggleMediaEntry called invalidly", entry);
     return;
   }
-  // console.log("ToggleMediaEntry called with ", entry);
   let store = await browser.storage.local.get("media");
   let media = store.media;
   if (!media) {
@@ -314,7 +277,6 @@ async function toggleMediaEntry(entry) {
   }
   let res = false;
   const index = media.findIndex((s) => entry === s);
-  // console.log("ToggleMediaEntry current state, index ", media, index);
   if (index === -1) {
     media.push(entry);
     res = true;
@@ -322,7 +284,6 @@ async function toggleMediaEntry(entry) {
     media.splice(index, 1);
   }
   await browser.storage.local.set({ media });
-  // console.log("ToggleMediaEntry, new state, retval: ", media, res);
   return res;
 }
 
@@ -332,14 +293,13 @@ async function getMedia() {
   return media;
 }
 
-// Get all sublists for a key
 async function getSpeaker(speaker) {
   let store = await browser.storage.local.get("lists");
   let lists = store.lists;
-  // console.log(lists.lists)
   return lists && lists[speaker] ? lists[speaker] : null;
 }
 
+// main body
 if (isMovie || isSeries) {
   createSpeakerStars();
   createMediaStar();
